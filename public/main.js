30fadfe2426995381bf54ffd72a65ba177714c50
Cat Goncalves <catgoncalves.dev@gmail.com>
2021-10-23 20:03:20 -0400

    Inventory Express App

---
+++ public/main.js
@@ -0,0 +1,45 @@
+var trash = document.getElementsByClassName("fa-trash");
+
+// Array.from(thumbUp).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const msg = this.parentNode.parentNode.childNodes[3].innerText
+//         const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+//         fetch('messages', {
+//           method: 'put',
+//           headers: {'Content-Type': 'application/json'},
+//           body: JSON.stringify({
+//             'name': name,
+//             'msg': msg,
+//             'thumbUp':thumbUp
+//           })
+//         })
+//         .then(response => {
+//           if (response.ok) return response.json()
+//         })
+//         .then(data => {
+//           console.log(data)
+//           window.location.reload(true)
+//         })
+//       });
+// });
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const item = this.parentNode.parentNode.childNodes[1].innerText
+        const price = this.parentNode.parentNode.childNodes[3].innerText
+        console.log(this.parentNode.parentNode.childNodes)
+        fetch('/inventory', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'items': item,
+            'price': price
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+var db, collection;
+
+const url = "mongodb+srv://test:test@cluster0.me8ts.mongodb.net/inventory?retryWrites=true&w=majority";
+const dbName = "inventory";
+
+app.listen(3000, () => {
+    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        db = client.db(dbName);
+        console.log("Connected to `" + dbName + "`!");
+    });
+});
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  db.collection('items').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {items: result})
+  })
+})
+
+app.post('/inventory', (req, res) => {
+  db.collection('items').insertOne({
+    item: req.body.item, 
+    price: req.body.price, 
+    
+  }, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+app.put('/inventory', (req, res) => {
+  db.collection('items')
+  .findOneAndUpdate({items: req.body.item, price: req.body.price}, {
+    $set: {
+      thumbUp:req.body.thumbUp + 1
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.delete('/inventory', (req, res) => {
+  db.collection('items').findOneAndDelete({items: req.body.item, price: req.body.price}, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('Message deleted!')
+  })
+})
